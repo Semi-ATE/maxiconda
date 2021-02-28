caa133080d54c32a907d2076feb237e2b73fd199
nerohmot <horen.tom@gmail.com>
2021-02-28 16:57:18 +0100

    again

--- scripts/build.sh
+++ scripts/build.sh
@@ -51,7 +51,7 @@ else
    EXT=sh;
 fi
 # This line will break if there is more than one installer in the folder.
-ls -la
+tree
 INSTALLER_PATH=$(find . -name "maxiconda*.$EXT" | head -n 1)
 HASH_PATH="$INSTALLER_PATH.sha256"
 sha256sum $INSTALLER_PATH > $HASH_PATH
