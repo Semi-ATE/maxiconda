e1170c6d889512eba66ae95367ca4435c93c79f5
nerohmot <horen.tom@gmail.com>
2021-03-05 08:39:09 +0100

    tracking windows version

--- build_maxiconda_windows.sh
+++ build_maxiconda_windows.sh
@@ -1,5 +1,5 @@
 conda install posix --yes
-echo "MAXICONDA_VERSION = $MAXICONDA_VERSION"
-echo "MAXICONDA_NAME = $MAXICONDA_NAME"
+echo "MAXICONDA_VERSION = '$MAXICONDA_VERSION'"
+echo "MAXICONDA_NAME = '$MAXICONDA_NAME'"
 source scripts/build.sh
 source scripts/test.sh
