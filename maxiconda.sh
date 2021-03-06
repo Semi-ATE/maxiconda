6a94b6c042956e6def81ec1d94c837851592c8d7
nerohmot <horen.tom@gmail.com>
2021-03-06 16:03:35 +0100

    enabled maxiconda.sh

--- maxiconda.sh
+++ maxiconda.sh
@@ -72,34 +72,34 @@ SHA256SUM="sha256sum ./$INSTALLER"
 
 printf "\\nStep #1: Downloading '$INSTALLER'\\n"
 printf -- "-------------------------------------------------------------------------------\\n"
-# $DOWNLOAD_INSTALLER
+$DOWNLOAD_INSTALLER
 
-# printf "\\nStep #2: Downloading '$SHA256'\\n"
-# printf -- "-------------------------------------------------------------------------------\\n"
-# $DOWNLOAD_SHA256
+printf "\\nStep #2: Downloading '$SHA256'\\n"
+printf -- "-------------------------------------------------------------------------------\\n"
+$DOWNLOAD_SHA256
 
-# printf "\\nStep #3: Checking installer integrity ..."
-# CALCULATED_CHECKSUM=`$SHA256SUM`
-# GIVEN_CHECKSUM=$(head -n 1 $SHA256)
-# if [ "$CALCULATED_CHECKSUM" == "$GIVEN_CHECKSUM" ]
-# then
-#     printf "PASS\\n"
-# else
-#     printf "FAIL:\\n"
-#     printf "Calculated: '%s'\\n" $CALCULATED_CHECKSUM
-#     printf "     Given: '%s'\\n" $GIVEN_CHECKSUM
-#     exit 1
-# fi
-# printf -- "-------------------------------------------------------------------------------\\n"
+printf "\\nStep #3: Checking installer integrity ..."
+CALCULATED_CHECKSUM=`$SHA256SUM`
+GIVEN_CHECKSUM=$(head -n 1 $SHA256)
+if [ "$CALCULATED_CHECKSUM" == "$GIVEN_CHECKSUM" ]
+then
+    printf "PASS\\n"
+else
+    printf "FAIL:\\n"
+    printf "Calculated: '%s'\\n" $CALCULATED_CHECKSUM
+    printf "     Given: '%s'\\n" $GIVEN_CHECKSUM
+    exit 1
+fi
+printf -- "-------------------------------------------------------------------------------\\n"
 
-# printf "\\nStep #4: Installing '$INSTALLER $PTA'\\n"
-# printf -- "-------------------------------------------------------------------------------\\n"
-# $RUN_INSTALLER
+printf "\\nStep #4: Installing '$INSTALLER $PTA'\\n"
+printf -- "-------------------------------------------------------------------------------\\n"
+$RUN_INSTALLER
 
-# printf "\\nStep #5: Cleaning up ... "
-# `rm -f $INSTALLER`
-# `rm -f $SHA256`
-# printf "Done.\\n"
-# printf -- "-------------------------------------------------------------------------------\\n"
+printf "\\nStep #5: Cleaning up ... "
+`rm -f $INSTALLER`
+`rm -f $SHA256`
+printf "Done.\\n"
+printf -- "-------------------------------------------------------------------------------\\n"
 
-# printf "\\nYou are all set and ready to go!\\n"
+printf "\\nYou are all set and ready to go!\\n"
