a5941446252a86ac7ece04cdc0425ac0323a85ee
Tom Hören <horen.tom@gmail.com>
2021-03-07 01:48:22 +0100

    Update build_maxiconda_macos.sh

--- build_maxiconda_macos.sh
+++ build_maxiconda_macos.sh
@@ -3,14 +3,14 @@
 set -e
 set -x
 
-echo "Installing a fresh version of maxiconda."
-MAXICONDA_URL="https://github.com/conda-forge/maxiconda/releases/download/4.8.3-1"
-MAXICONDA_FILE="maxiconda-4.8.3-1-MacOSX-x86_64.sh"
-curl -L -O "${MAXICONDA_URL}/${MAXICONDA_FILE}"
-bash $MAXICONDA_FILE -b
+echo "Installing a fresh version of Miniforge3."
+MINIFORGE_URL="https://github.com/conda-forge/miniforge/releases/download/4.8.3-1"
+MINIFORGE_FILE="Miniforge3-4.8.3-1-MacOSX-x86_64.sh"
+curl -L -O "${MINIFORGE_URL}/${MINIFORGE_FILE}"
+bash $MINIFORGE_FILE -b
 
 echo "Configuring conda."
-source ~/maxiconda/bin/activate root
+source ~/miniforge3/bin/activate root
 
 export CONSTRUCT_ROOT=$PWD
 mkdir -p build
