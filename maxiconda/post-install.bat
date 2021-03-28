2890af8cb61c2565bc6d84bd0838a13e785efa38
nerohmot <horen.tom@gmail.com>
2021-03-28 09:59:05 +0200

    post-install

--- maxiconda/post-install.bat
+++ maxiconda/post-install.bat
@@ -8,8 +8,17 @@
 # this script is (on Windows) responsible to create the '_spyder_' and 'maxiconda'
 # environments with the respectively named conda metapackages from the Semi-ATE channel.
 #
-
 @echo off
-setlocal enabledelayedexpansion
-echo maxclients !%PREFIX%! > bla.cfg
-REM call activate ?!?
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> start : post-install <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+
+call %PREFIX%\maxiconda\Scripts\activate.bat
+call conda config --set channel_priority strict
+call conda config --append channels Semi-ATE
+
+REM call mamba create -n _spyder_ _spyder_ -y
+call mamba create -n _spyder_ -c conda-forge/label/beta spyder=5 ffmpeg -y
+
+REM call mamba create -n maxiconda maxiconda -y
+call mamba create -n maxiconda numpy ipython -y
+
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> end : post-install <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
