fa197885f541ebc8463cb67373dfbc8fcd20adb0
Tom Hören <horen.tom@gmail.com>
2021-04-25 14:57:58 +0200

    Update post-install.bat

--- maxiconda/post-install.bat
+++ maxiconda/post-install.bat
@@ -1,15 +1,17 @@
-# Copyright (c) Semi-ATE
-# Distributed under the terms of the BSD 3-Clause License
+REM Copyright (c) Semi-ATE
+REM Distributed under the terms of the BSD 3-Clause License
 
-# file : post-install.bat
+REM file : post-install.bat
 
-# 
-# Once the maxiconda installer did run (and thus created the 'base' envriornment),
-# this script is (on Windows) responsible to create the '_spyder_' and 'maxiconda'
-# environments with the respectively named conda metapackages from the Semi-ATE channel.
-#
+REM 
+REM Once the maxiconda installer did run (and thus created the 'base' envriornment),
+REM this script is (on Windows) responsible to create the '_spyder_' and 'maxiconda'
+REM environments with the respectively named conda metapackages from the Semi-ATE channel.
+REM
 @echo off
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> start : post-install <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+
+set
 
 call %PREFIX%\maxiconda\Scripts\activate.bat
 call conda config --set channel_priority strict
