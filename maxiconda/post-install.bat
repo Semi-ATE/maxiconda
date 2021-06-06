de7c60601509f609ba55b4748592c088a73b8dd5
Tom Hören <horen.tom@gmail.com>
2021-06-06 20:01:07 +0200

    Update post-install.bat

--- maxiconda/post-install.bat
+++ maxiconda/post-install.bat
@@ -1,15 +1,6 @@
-echo >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> start : post-install <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
-echo %PREFIX%
-
-set /p DUMMY=Hit ENTER to continue...
-
 call %PREFIX%\condabin\activate.bat
 call conda config --set channel_priority strict
 call conda config --append channels Semi-ATE
-
-call mamba create -n _spyder_ spyder>=5 ffmpeg spyder-screencast spyder-remote-client -y
-
-call mamba create -n maxiconda numpy ipython maxiconda-shortcuts -y
-
-echo >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> end : post-install <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+call mamba create -n _spyder_ spyder ffmpeg spyder-screencast spyder-remote-client -y
+call mamba create -n maxiconda numpy ipython -y
+call mamba install maxiconda-shortcuts -y
