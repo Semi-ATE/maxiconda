04716926c2d356027cd1a9121581c4097597240c
Tom Hören <horen.tom@gmail.com>
2021-07-07 15:02:30 +0200

    Update post-install.bat

--- maxiconda/post-install.bat
+++ maxiconda/post-install.bat
@@ -1,7 +1,7 @@
 call %PREFIX%\condabin\activate.bat
 call conda config --set channel_priority strict
 call conda config --append channels Semi-ATE
-REM call mamba install maxiconda-base
 call mamba create -n _spyder_ _spyder_ -y
 call mamba create -n maxiconda maxiconda -y
-call mamba install maxiconda-shortcuts spyder-remote-server -y
+call mamba install menuinst maxiconda-shortcuts spyder-remote-server -y
+REM need to activate the spyder-remote-server here ...
