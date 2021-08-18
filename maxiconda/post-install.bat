cc9334866f94925ef04bd572b03ddf34b9b79b32
Tom Hören <horen.tom@gmail.com>
2021-08-18 10:13:04 +0200

    Update post-install.bat

--- maxiconda/post-install.bat
+++ maxiconda/post-install.bat
@@ -1,6 +1,8 @@
 call %PREFIX%\condabin\activate.bat
 call conda config --set channel_priority strict
+call conda config --remove channels default
 call conda config --prepend channels Semi-ATE
+call conda config --append channels conda-forge
 call mamba create -n _spyder_ _spyder_ -y
 call mamba create -n maxiconda maxiconda -y
 call mamba install menuinst maxiconda-shortcuts spyder-remote-server -y
